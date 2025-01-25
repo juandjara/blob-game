3a7df43434588c951f15c41bdec631b98d22a7ad
Hiriad <157838411+Hiriad@users.noreply.github.com>
2025-01-25 11:17:27 +0100

    Pause Menu Added
    added pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public static bool PausePressed = false;
+
+    public GameObject pauseMenuUI;
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!PausePressed) return;
+        Pause();
+        PausePressed = false;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
