e5109b2b6b8e29653124c6986808ec2f1056c36d
Alberto Alan Sánchez <al404015@uji.es>
2025-01-25 17:41:13 +0100

    bubbles spawners

---
+++ Assets/Scripts/Bubbles/BubbleMovement.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using Random = UnityEngine.Random;
+using Vector2 = System.Numerics.Vector2;
+
+public class BubbleMovement : MonoBehaviour
+{
+ 
+    [SerializeField] private float _speed=0.5f;
+    public int Inverse;
+    public Vector3 CannonRotation = new Vector3(0,0,0);
+
+    private int _angle;
+
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        _angle = Random.Range(-60, 60);
+       BubbleMoveRandomDirectionStraight();
+    }
+    
+   private void FixedUpdate()
+    {
+        //fisicas
+        Debug.Log(_angle);
+        
+        var lDirection = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * _angle), Mathf.Cos(Mathf.Deg2Rad * _angle));
+
+        rb.linearVelocity = (transform.up+lDirection) * _speed;
+    }
+   
+    public void BubbleMoveRandomDirectionStraight()
+    {
+        
+        
+        //animacion
+        switch (_angle)
+        {
+            case <-30:
+                Debug.Log("animacion izquierda");
+                break;
+            case >30:
+                Debug.Log("animacion derecha");
+                break;
+            default:
+                Debug.Log("animación centro");
+                break;
+        }
+        
+       
+    }
+}
