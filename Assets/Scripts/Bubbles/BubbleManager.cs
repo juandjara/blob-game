a654c041b70d1471ec569a75c519b7cd549f021b
Alberto Alan Sánchez <al404015@uji.es>
2025-01-26 02:22:35 +0100

    animations

--- Assets/Scripts/Bubbles/BubbleManager.cs
+++ Assets/Scripts/Bubbles/BubbleManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UIElements;
 public class BubbleManager : MonoBehaviour
 {
     [SerializeField] private GameObject bubblePrfab;
+    [SerializeField] private GameObject animationPrefab;
     [SerializeField] private GameObject[] spawnPoints;
     
     [SerializeField] private float spawnTime = 2f;
@@ -15,7 +16,10 @@ public class BubbleManager : MonoBehaviour
     Quaternion _currentRotation;
     private Vector3 _currentPosition;
     
-    
+    private const int animAngleMin = -30;
+    private const int animAngleMax = 30;
+
+    private Animator _animator;
     
     void Start()
     {
@@ -30,8 +34,7 @@ public class BubbleManager : MonoBehaviour
             spawnTimer -= Time.deltaTime;
             if (spawnTimer <= 0)
             {
-                CreateBubble(Random.Range(0, spawnPoints.Length-1));
-
+                CreateBubble(Random.Range(0, spawnPoints.Length ));
                 spawnTimer += spawnTime;
             }
         }
@@ -46,9 +49,36 @@ public class BubbleManager : MonoBehaviour
         bubble.CannonRotation = rotation;
        */
        
-        Instantiate(bubblePrfab, spawnPoints[pt].transform.position, spawnPoints[pt].transform.rotation);
+        GameObject bubble= Instantiate(bubblePrfab, spawnPoints[pt].transform.position, spawnPoints[pt].transform.rotation);
+        GameObject animation = Instantiate(animationPrefab, spawnPoints[pt].transform.position,
+            spawnPoints[pt].transform.rotation);
+        _animator = animation.GetComponent<Animator>();
+        //Debug.Log("angle :) " +bubble.GetComponent<BubbleMovement>().GetAngle());
+        BubbleMoveRandomDirectionStraight(bubble.GetComponent<BubbleMovement>().GetAngle());
     } 
     
+    public void BubbleMoveRandomDirectionStraight(int _angle)
+    {
+        
+        //animacion
+        switch (_angle)
+        {
+            case < animAngleMin:
+                _animator.SetInteger("Posicion",-1);
+                Debug.Log("animacion izquierda");
+                break;
+            case > animAngleMax:
+                _animator.SetInteger("Posicion",1);
+                Debug.Log("animacion derecha");
+                break;
+            default:              
+                _animator.SetInteger("Posicion",0);
+                Debug.Log("animación centro");
+                break;
+        }
+        
+       
+    }
     
     public void setBubbleFrecuency(float time)
     {
--- Assets/Scripts/Bubbles/BubbleMovement.cs
+++ Assets/Scripts/Bubbles/BubbleMovement.cs
@@ -5,6 +5,7 @@ using Random = UnityEngine.Random;
 public class BubbleMovement : MonoBehaviour
 {
 
+    [SerializeField] private bool move = false;
     [SerializeField] private Animator _animator;
     
     [SerializeField] private float _speed=0.5f;
@@ -14,25 +15,24 @@ public class BubbleMovement : MonoBehaviour
     [SerializeField] private int shootAngleMin = -60;
     [SerializeField] private int shootAngleMax = 60;
                              
-    private const int animAngleMin = -30;
-    private const int animAngleMax = 30;
-
+    
+    
     private int _angle;
 
     public Rigidbody2D rb;
 
-    private void Awake()
-    {
-        BubbleMoveRandomDirectionStraight();
-    }
-
+ 
     private void Start()
     {
         _angle = Random.Range(shootAngleMin, shootAngleMax);
         Debug.Log("Bubble angle:" +_angle);
        
     }
-    
+
+    public void MoveBubble()
+    {
+        move = true;
+    }
    private void FixedUpdate()
     {
         //fisicas
@@ -40,34 +40,15 @@ public class BubbleMovement : MonoBehaviour
         
         //var lDirection = new Vector3(0, Mathf.Sin(Mathf.Deg2Rad * _angle), Mathf.Cos(Mathf.Deg2Rad * _angle));
          
-        rb.linearVelocity = (rotateVector(transform.up,_angle)) * _speed;
+        /*if(move)*/ rb.linearVelocity = /*(rotateVector(transform.up,_angle)) */transform.up * _speed;
         
         //rb.linearVelocity = (transform.up+lDirection) * _speed;
 
     }
-   
-    public void BubbleMoveRandomDirectionStraight()
+
+    public int GetAngle()
     {
-        
-        //animacion
-        switch (_angle)
-        {
-            case < animAngleMin:
-                _animator.SetInteger("Posicion",-1);
-                Debug.Log("animacion izquierda");
-                break;
-            case > animAngleMax:
-                _animator.SetInteger("Posicion",1);
-                Debug.Log("animacion derecha");
-                break;
-            default:              
-                _animator.SetInteger("Posicion",0);
-                Debug.Log("animación centro");
-                break;
-        }
-        
-       
-    }
+        return _angle;}
     private Vector2 rotateVector(Vector2 vector, float angle)
     {
         float rad = angle * Mathf.Deg2Rad;
